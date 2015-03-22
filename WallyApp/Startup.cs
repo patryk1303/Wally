8727ff6b1b645bda99b704bf660cdee8251244fb
Jarosław Rytlewski <jaroslaw.rytlewski@gmail.com>
2015-03-22 14:04:29 +0100

    Project files

---
+++ WallyApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WallyApp.Startup))]
+namespace WallyApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
