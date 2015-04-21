b134cc2a71863c6e0f0ff75c13fa6a166e774d11
patryk1303 <patryk1303@gmail.com>
2015-04-21 04:07:43 +0200

    Initial support for groups

---
+++ WallyApp/Models/GroupModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WallyApp.Models
+{
+    public class GroupModels
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Owner { get; set; }
+    }
+
+    public class GroupCreateViewModel
+    {
+        [Required]
+        [Display(Name="Name")]
+        public string Name { get; set; }
+    }
+}
